aeed63d222bf1238be9403ed1a54ed066d7b8f42
Fabrizio Duroni <fabrizio.duroni@gmail.com>
2025-01-18 22:21:12 +0100

    feat(migration): Gatsby to NextJS
    Added google tag manager.

--- TODO.md
+++ TODO.md
@@ -1,6 +1,6 @@
-[ ] JSON LD for all pages
+[X] JSON LD for all pages
 [X] Search
-[ ] Google Tag Manager https://nextjs.org/docs/app/building-your-application/optimizing/third-party-libraries#google-tag-manager
+[X] Google Tag Manager https://nextjs.org/docs/app/building-your-application/optimizing/third-party-libraries#google-tag-manager
 [ ] Sitemap
 [ ] Manifest
 [ ] Favicon + Gatsby static folder content
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,30 +1,30 @@
-import {ReactNode} from "react";
+import { ReactNode } from "react";
 import StyledComponentsRegistry from "@/lib/styled-components-registry";
-import {ThemePage} from "@/components/templates/theme-page";
-import {blogTheme} from "@/components/design-system/themes/theme";
-import {CookieConsent} from "@/components/website/CookieConsent";
-import {Viewport} from "next";
-import {SharedHead} from "@/components/website/SharedHead";
+import { ThemePage } from "@/components/templates/theme-page";
+import { blogTheme } from "@/components/design-system/themes/theme";
+import { Viewport } from "next";
+import { SharedHead } from "@/components/website/share-head";
+import { CookieConsent } from "@/components/website/cookie-consent";
+import { GoogleTagManager } from "@next/third-parties/google";
 
 export const viewport: Viewport = {
-    width: 'device-width',
-    initialScale: 1
-}
+  width: "device-width",
+  initialScale: 1,
+};
 
 export default function RootLayout({
-    children
+  children,
 }: Readonly<{ children: ReactNode }>) {
-    return (
-        <html lang="en">
-            <SharedHead />
-            <body>
-                <StyledComponentsRegistry>
-                    <ThemePage theme={blogTheme}>
-                        {children}
-                    </ThemePage>
-                    <CookieConsent/>
-                </StyledComponentsRegistry>
-                </body>
-        </html>
-    );
+  return (
+    <html lang="en">
+      <GoogleTagManager gtmId={"G-B992TEM300"} />
+      <SharedHead />
+      <body>
+        <StyledComponentsRegistry>
+          <ThemePage theme={blogTheme}>{children}</ThemePage>
+          <CookieConsent />
+        </StyledComponentsRegistry>
+      </body>
+    </html>
+  );
 }
