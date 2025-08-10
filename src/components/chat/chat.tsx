50e2a3c9f5c441443872347e93cad30b8e5c39d9
Fabrizio Duroni <fabrizio.duroni@gmail.com>
2025-08-10 19:25:54 +0200

    Fix bottom bar ios :rocket:

--- src/components/chat/chat.tsx
+++ src/components/chat/chat.tsx
@@ -9,15 +9,20 @@ import { ChatHeader } from "@/components/chat/components/chat-header";
 import { ChatWelcome } from "@/components/chat/components/chat-welcome";
 import { ChatMessage } from "@/components/chat/components/chat-message";
 import { ChatInput } from "./components/chat-input";
+import { mediaQuery } from "@/components/design-system/utils-css/media-query";
 
 const MessagesContainer = styled.div`
   flex: 1;
   overflow-y: auto;
-  padding: 160px 0 ${(props) => props.theme.spacing[12]} 0;
+  padding: 160px 0 calc(140px + env(safe-area-inset-bottom, 0px)) 0;
   display: flex;
   flex-direction: column;
   gap: ${(props) => props.theme.spacing[3]};
   scroll-behavior: smooth;
+
+  ${mediaQuery.minWidth.sm} {
+    padding: 160px 0 ${(props) => props.theme.spacing[12]} 0;
+  }
 
   &::-webkit-scrollbar {
     display: none;
