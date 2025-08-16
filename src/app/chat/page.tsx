b2b9a760ca6f673db8dd4d471b436215abcdcf96
Fabrizio Duroni <fabrizio.duroni@gmail.com>
2025-08-17 01:15:38 +0200

    feat(matrix): refactor chat metadata :rocket:

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,23 +1,20 @@
 import { createMetadata } from "@/lib/seo/seo";
-import { Metadata } from "next";
 import { siteMetadata } from "@/types/site-metadata";
 import { tracking } from "@/types/tracking";
 import { slugs } from "@/types/slug";
 import { Chat } from "@/components/chat/components/chat";
 import { BasicPageTemplate } from "@/components/templates/basic-page-template";
 
-export async function generateMetadata(): Promise<Metadata> {
-  return {
-    ...createMetadata({
-      author: siteMetadata.author,
-      title: siteMetadata.title,
-      url: `${siteMetadata.siteUrl}${slugs.chat}`,
-      imageUrl: siteMetadata.featuredImage,
-      ogPageType: "website",
-    }),
-    robots: { index: false, follow: false },
-  };
-}
+export const metadata = {
+  ...createMetadata({
+    author: siteMetadata.author,
+    title: siteMetadata.title,
+    url: `${siteMetadata.siteUrl}${slugs.chat}`,
+    imageUrl: siteMetadata.featuredImage,
+    ogPageType: "website",
+  }),
+  robots: { index: false, follow: false },
+};
 
 export default function ChatInterface() {
   return (
