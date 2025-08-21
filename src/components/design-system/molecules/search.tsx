3f51c38295b18f1d1dd7abaa2c8fb324f742da80
Fabrizio Duroni <fabrizio.duroni@gmail.com>
2025-08-21 21:54:30 +0200

    feat(matrix): fix layout for search :rocket:

--- src/components/design-system/molecules/search.tsx
+++ src/components/design-system/molecules/search.tsx
@@ -1,21 +1,21 @@
-'use client'
+"use client";
 
-import React, {ChangeEvent, FC} from "react";
-import styled, {css, TransientProps} from "styled-components";
-import {SearchAlt} from "@styled-icons/boxicons-regular";
-import {mediaQuery} from "../utils/media-query";
-import {Container} from "../atoms/container";
-import {List} from "../atoms/list";
-import {Paragraph} from "../atoms/paragraph";
-import {borderRadius} from "../atoms/border-radius";
-import {SearchablePostFields} from "@/types/post";
+import React, { ChangeEvent, FC } from "react";
+import styled, { css, TransientProps } from "styled-components";
+import { SearchAlt } from "@styled-icons/boxicons-regular";
+import { mediaQuery } from "../utils/media-query";
+import { Container } from "../atoms/container";
+import { List } from "../atoms/list";
+import { Paragraph } from "../atoms/paragraph";
+import { borderRadius } from "../atoms/border-radius";
+import { SearchablePostFields } from "@/types/post";
 import Link from "next/link";
 import { hideScrollbar } from "../utils/components/hide-scrollbar";
 import { glassmorphism } from "../atoms/glassmorphism";
 
 const SearchListContainer = styled(Container)`
   position: absolute;
-  z-index: 100;
+  backdrop-filter: blur(1px);
   top: 100px;
   right: 0;
   left: 0;
@@ -23,17 +23,18 @@ const SearchListContainer = styled(Container)`
   height: 80vh;
   overflow: scroll;
   ${hideScrollbar};
-  padding: ${(props) => props.theme.spacing[2]}; /* Aumentato per contenere il glow effect */
+  padding: ${(props) =>
+    props.theme.spacing[2]}; /* Aumentato per contenere il glow effect */
 
   body.scroll-locked & {
     right: var(--scrollbar-width, 0px);
   }
-  
-  box-shadow: 
+
+  box-shadow:
     inset 0 0 3px ${(props) => props.theme.dark.accentColor}AA,
     inset 0 0 8px ${(props) => props.theme.dark.accentColor}99,
     inset 0 0 10px ${(props) => props.theme.dark.accentColor}55;
-  
+
   border: 1px solid ${(props) => props.theme.dark.accentColor}20;
   border-radius: ${(props) => props.theme.spacing[2]};
 
@@ -45,38 +46,36 @@ const SearchListContainer = styled(Container)`
 `;
 
 const SearchHitsList = styled(List)`
-  list-style: none;
   padding: 0;
   margin: 0;
   display: flex;
   flex-direction: column;
   gap: ${(props) => props.theme.spacing[2]};
-  
-  /* Container più piccolo per contenere il glow effect */
+
   margin: 0 auto;
 
-  /* Override del before del List component per rimuovere le frecce */
   li::before {
     content: none;
   }
-  
+
   li {
-    padding-left: 0; /* Rimuove il padding-left del List component */
-    margin-bottom: 0; /* Rimuove il margin-bottom del List component */
+    padding-left: 0;
+    margin-bottom: 0;
   }
 `;
 
 const SearchHitCard = styled.li`
   ${glassmorphism};
-  padding: ${(props) => props.theme.spacing[2]}; /* Aumentato per più spazio interno */
+  padding: ${(props) =>
+    props.theme.spacing[2]}; /* Aumentato per più spazio interno */
   transition: all 0.3s cubic-bezier(0.4, 0, 0.2, 1);
-  
+
   /* Assicura che il contenuto sia allineato a sinistra */
   text-align: left;
-  
+
   &:hover {
     transform: translateY(-2px);
-    box-shadow: 
+    box-shadow:
       0 12px 40px ${(props) => props.theme.dark.boxShadowLight},
       0 0 10px ${(props) => props.theme.dark.accentColor}33,
       inset 0 1px 0 ${(props) => props.theme.dark.accentColor}26;
@@ -103,9 +102,9 @@ const SearchTitle = styled(Paragraph)`
   margin-bottom: ${(props) => props.theme.spacing[2]};
   line-height: 1.3;
   margin: 0 0 ${(props) => props.theme.spacing[2]} 0; /* Reset margin per allineamento pulito */
-  
+
   /* Matrix glow effect */
-  text-shadow: 
+  text-shadow:
     0 0 5px ${(props) => props.theme.dark.accentColor}40,
     0 0 10px ${(props) => props.theme.dark.accentColor}20;
 
@@ -119,7 +118,7 @@ const SearchDescription = styled(Paragraph)`
   opacity: 0.9;
   line-height: 1.5;
   margin: 0;
-  
+
   /* Subtle glow for description */
   text-shadow: 0 0 3px ${(props) => props.theme.dark.accentColor}15;
 `;
@@ -186,7 +185,8 @@ interface OnClickProp {
 }
 
 export const SearchBox: FC<
-  StartSearchProps & OnClickProp & { onChange: (e: ChangeEvent<HTMLInputElement>) => void }
+  StartSearchProps &
+    OnClickProp & { onChange: (e: ChangeEvent<HTMLInputElement>) => void }
 > = ({ startSearch, onClick, onChange }) => (
   <SearchBoxContainer>
     <SearchBoxInput
@@ -201,7 +201,9 @@ export const SearchBox: FC<
   </SearchBoxContainer>
 );
 
-export const SearchHits: FC<{ results: SearchablePostFields[] }> = ({ results }) => (
+export const SearchHits: FC<{ results: SearchablePostFields[] }> = ({
+  results,
+}) => (
   <SearchListContainer>
     <SearchHitsList>
       {results.map((result, index) => (
