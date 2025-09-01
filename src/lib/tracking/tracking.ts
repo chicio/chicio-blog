63d2bdd7509d13258c64993728916a1b3bc08c35
Fabrizio Duroni <fabrizio.duroni@gmail.com>
2025-09-01 09:12:55 +0200

    feat(tracking): using google analytics tracking :rocket:

--- src/lib/tracking/tracking.ts
+++ src/lib/tracking/tracking.ts
@@ -1,10 +1,11 @@
-import {TrackingData, TrackingPayload} from "@/types/tracking";
 import { hasConsented } from "@/lib/consents/consents";
+import { TrackingData, TrackingPayload } from "@/types/tracking";
+import { sendGAEvent } from "@next/third-parties/google";
 
 export const trackWith = (tracking: TrackingData) => {
     const payload: TrackingPayload = { event_category: tracking.category, event_label: tracking.label }
 
-    if (typeof window !== "undefined" && typeof window.gtag !== "undefined" && hasConsented()) {
-        window.gtag("event", tracking.action, payload);
+    if (hasConsented()) {
+        sendGAEvent('event', tracking.action, payload);
     }
 };
