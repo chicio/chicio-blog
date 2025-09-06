3f5b9b7f86d3b3963c9c56eccea67834ea5e473e
Fabrizio Duroni <fabrizio.duroni@gmail.com>
2025-09-06 22:50:51 +0200

    feat(next): added config for redirect from old post format :rocket:

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,23 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    compiler: {
-        styledComponents: true,
-    },
-    async redirects() {
-        return [
-            {
-                source: '/:year/:month/:day/:slug',
-                destination: '/blog/post/:year/:month/:day/:slug',
-                permanent: true,
-            },
-        ]
-    },
+  compiler: {
+    styledComponents: true,
+  },
+  async redirects() {
+    return [
+      {
+        source: "/:year/:month/:day/:slug.html",
+        destination: "/:year/:month/:day/:slug",
+        permanent: true,
+      },
+      {
+        source: "/:year/:month/:day/:slug",
+        destination: "/blog/post/:year/:month/:day/:slug",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
