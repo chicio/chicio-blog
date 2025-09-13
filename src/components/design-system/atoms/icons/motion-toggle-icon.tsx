49d95198bbb22499df2af9dcae5744be7b177312
Fabrizio Duroni <fabrizio.duroni@gmail.com>
2025-09-13 17:55:08 +0200

    feat(ux): :lipstick: added pulse animation to motion button

--- src/components/design-system/atoms/icons/motion-toggle-icon.tsx
+++ src/components/design-system/atoms/icons/motion-toggle-icon.tsx
@@ -2,13 +2,13 @@ import { MdAnimation, MdDoDisturb } from "react-icons/md";
 import { RoundedIcon } from "./icon";
 
 export const MotionOffIcon = () => (
-  <RoundedIcon className="flex items-center justify-center text-text-above-primary">
+  <RoundedIcon className="animate-pulse flex items-center justify-center text-text-above-primary">
     <MdDoDisturb className="size-5 md:size-7" title="Animazioni disattivate" />
   </RoundedIcon>
 );
 
 export const MotionOnIcon = () => (
-  <RoundedIcon className="flex items-center justify-center text-text-above-primary">
+  <RoundedIcon className="animate-pulse flex items-center justify-center text-text-above-primary">
     <MdAnimation className="size-5 md:size-7" title="Animazioni attivate" />
   </RoundedIcon>
 );
