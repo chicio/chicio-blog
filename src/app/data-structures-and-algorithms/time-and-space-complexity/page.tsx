a81acb551ca7179a2b495f848a2c19bfbc296ed8
Fabrizio Duroni <fabrizio.duroni@gmail.com>
2025-10-31 19:41:05 +0100

    refactor(capabilities): :recycle: refactor dsa pages with template component

--- src/app/data-structures-and-algorithms/time-and-space-complexity/page.tsx
+++ src/app/data-structures-and-algorithms/time-and-space-complexity/page.tsx
@@ -1,18 +1,10 @@
-import "highlight.js/styles/tokyo-night-dark.css";
-import "katex/dist/katex.min.css";
-
-import { ReadingContentPageTemplate } from "@/components/design-system/templates/reading-content-page-template";
-import { siteMetadata } from "@/types/site-metadata";
-import { tracking } from "@/types/tracking";
+import { DsaPage } from "@/components/sections/dsa/components/dsa-page";
 import TimeAndSpaceComplexity from "../../../content/dsa/time-and-space-complexity.mdx";
 
 export default function TimeAndSpaceComplexityPage() {
   return (
-    <ReadingContentPageTemplate
-      author={siteMetadata.author}
-      trackingCategory={tracking.category.dsa}
-    >
+    <DsaPage>
       <TimeAndSpaceComplexity />
-    </ReadingContentPageTemplate>
+    </DsaPage>
   );
 }
