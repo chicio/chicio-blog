e78ce7d05cc955f0d66de865bde209e1e169e0e2
Fabrizio Duroni <fabrizio.duroni@gmail.com>
2025-11-06 23:33:00 +0100

    feat(content): :sparkles: hashtable page setup

---
+++ src/app/data-structures-and-algorithms/hashtable/page.tsx
@@ -0,0 +1,10 @@
+import { DsaPage } from "@/components/sections/dsa/components/dsa-page";
+import BitManipulation from "../../../content/dsa/hashtable.mdx";
+
+export default function HashtablePage() {
+  return (
+    <DsaPage>
+      <Hashtable />
+    </DsaPage>
+  );
+}
