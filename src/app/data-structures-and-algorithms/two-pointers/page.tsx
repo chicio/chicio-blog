164eb4c28d40bcf0ea63283e44c9e3d144a94387
Fabrizio Duroni <fabrizio.duroni@gmail.com>
2025-11-10 22:33:18 +0100

    feat(content): :sparkles: setup two pointers page

---
+++ src/app/data-structures-and-algorithms/two-pointers/page.tsx
@@ -0,0 +1,10 @@
+import { DsaPage } from "@/components/sections/dsa/components/dsa-page";
+import TwoPointers from "../../../content/dsa/two-pointers.mdx";
+
+export default function TwoPointersPage() {
+  return (
+    <DsaPage>
+      <TwoPointers />
+    </DsaPage>
+  );
+}
