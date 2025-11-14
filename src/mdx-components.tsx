81b564a58e245192295e51ff206e8bcc17c03681
Fabrizio Duroni <fabrizio.duroni@gmail.com>
2025-11-14 15:25:58 +0100

    fix(ux): :bug: table layout now is correct also on mobile devices

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -1,7 +1,13 @@
 import type { MDXComponents } from 'mdx/types'
- 
-const components: MDXComponents = {}
- 
+
+const components: MDXComponents = {
+    table: (props) => (
+        <div className="table-wrapper">
+            <table {...props} />
+        </div>
+    ),
+}
+
 export function useMDXComponents(): MDXComponents {
-  return components
+    return components
 }
