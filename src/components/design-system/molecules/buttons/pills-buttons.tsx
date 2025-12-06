428840b381efa9bf3e06f4ad4124d717ff8a55e2
Fabrizio Duroni <fabrizio.duroni@gmail.com>
2025-12-06 16:11:16 +0100

    feat(content): :sparkles: kadane's algorithm (#143)
    * feat(content): :sparkles: kadane's algorithm page setup
    
    * feat(content): :sparkles: paragraphs for kadane algorithm
    
    * feat(content): :sparkles: kadane's page ready to be published
    
    * feat(content): :sparkles: kadane's algorithm page linked to main dsa article

--- src/components/design-system/molecules/buttons/pills-buttons.tsx
+++ src/components/design-system/molecules/buttons/pills-buttons.tsx
@@ -5,16 +5,17 @@ import { BluePill, RedPill } from "../../atoms/effects/pills";
 
 type PillProps = PropsWithChildren<{
   onClick: () => void;
+  disabled?: boolean;
 }>;
 
-export const RedPillButton: FC<PillProps> = ({ children, onClick }) => (
-  <button className="bg-transparent border-none cursor-pointer p-0" onClick={onClick}>
+export const RedPillButton: FC<PillProps> = ({ children, onClick, disabled }) => (
+  <button className="bg-transparent border-none cursor-pointer p-0" onClick={onClick} disabled={disabled}>
     <RedPill>{children}</RedPill>
   </button>
 );
 
-export const BluePillButton: FC<PillProps> = ({ children, onClick }) => (
-  <button className="bg-transparent border-none cursor-pointer p-0" onClick={onClick}>
+export const BluePillButton: FC<PillProps> = ({ children, onClick, disabled }) => (
+  <button className="bg-transparent border-none cursor-pointer p-0" onClick={onClick} disabled={disabled}>
     <BluePill>{children}</BluePill>
   </button>
 );
