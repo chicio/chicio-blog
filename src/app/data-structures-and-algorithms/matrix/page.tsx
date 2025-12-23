a40c5a6134adc29c80963418061b4c087c07a59d
Fabrizio Duroni <fabrizio.duroni@gmail.com>
2025-12-23 12:35:53 +0100

    feat(content): :sparkles: setup matrix page

---
+++ src/app/data-structures-and-algorithms/matrix/page.tsx
@@ -0,0 +1,10 @@
+import { DsaPage } from "@/components/sections/dsa/components/dsa-page";
+import Matrix from "../../../content/dsa/matrix.mdx";
+
+export default function MatrixPage() {
+  return (
+    <DsaPage>
+      <Matrix />
+    </DsaPage>
+  );
+}
