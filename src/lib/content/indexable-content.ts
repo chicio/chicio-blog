d509d2196a64423016e8eeaca53d699882293d00
Fabrizio Duroni <fabrizio.duroni@gmail.com>
2026-02-25 00:23:22 +0100

    feat(content): :sparkles: game data for ps3 games + games are now indexed

--- src/lib/content/indexable-content.ts
+++ src/lib/content/indexable-content.ts
@@ -1,10 +1,13 @@
 import { getAboutMe } from "./about-me";
 import { getAllDataStructuresAndAlgorithmsTopics, getDataStructuresAndAlgorithmsRoadmap } from "./data-structures-and-algorithms";
 import { getPosts } from "./posts";
+import { getAllConsoles, getAllGames } from "./videogames";
 
 export const getIndexableContent = () => [
   ...getPosts(),
   ...getAllDataStructuresAndAlgorithmsTopics(),
+  ...getAllConsoles(),
+  ...getAllGames(),
   getDataStructuresAndAlgorithmsRoadmap(),
   getAboutMe(),
 ];
