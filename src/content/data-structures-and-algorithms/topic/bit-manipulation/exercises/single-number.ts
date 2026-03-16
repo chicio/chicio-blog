4fe53e94d1fcb7c3d5d593e1ad4be328c0f44f17
Fabrizio Duroni <fabrizio.duroni@gmail.com>
2026-03-16 21:26:55 +0100

    chore(content): converted exercises to mdx

--- src/content/data-structures-and-algorithms/topic/bit-manipulation/exercises/single-number.ts
+++
@@ -1,6 +0,0 @@
-/**
- * https://leetcode.com/problems/single-number/description/
- * 136. Single Number
- */
-
-const singleNumber = (nums: number[]) => nums.reduce((accumulator, current) => accumulator ^ current)
