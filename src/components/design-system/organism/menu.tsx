eab6372c28377ff22c176f0c1caccb5d52f12aa1
Fabrizio Duroni <fabrizio.duroni@gmail.com>
2026-06-13 18:17:22 +0200

    chore: removed menu entry

--- src/components/design-system/organism/menu.tsx
+++ src/components/design-system/organism/menu.tsx
@@ -166,17 +166,6 @@ const renderMenuItems = (
                         selected: pathname === slugs.contact,
                         onClickCallback: () => setShouldOpenMenu(false),
                     },
-                    {
-                        label: "Chat",
-                        to: slugs.chat,
-                        trackingData: {
-                            action: tracking.action.open_chat,
-                            category: trackingCategory,
-                            label: tracking.label.header,
-                        },
-                        selected: pathname === slugs.chat,
-                        onClickCallback: () => setShouldOpenMenu(false),
-                    },
                 ]}
             />
         </>
